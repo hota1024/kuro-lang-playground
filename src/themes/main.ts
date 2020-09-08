03845df89581640db9f7eca2c5032522f6115e3d
hota1024 <hotalog@hota1024.com>
2020-09-08 23:02:10 +0900

    🏷️ Set dark mode

--- src/themes/main.ts
+++ src/themes/main.ts
@@ -1,4 +1,12 @@
 import { createMuiTheme } from '@material-ui/core'
+import { deepPurple } from '@material-ui/core/colors'
 
-export const mainTheme = createMuiTheme({})
-
+export const mainTheme = createMuiTheme({
+  palette: {
+    type: 'dark',
+    primary: {
+      main: '#000000',
+    },
+    secondary: deepPurple,
+  },
+})
