5c4da47d9515513a84788df7f5affd67a7c7b67b
hota1024 <hotalog@hota1024.com>
2020-09-09 15:39:58 +0900

    🏷️ Add example menu

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,8 +5,12 @@ import {
   Toolbar,
   Typography,
   Button,
+  Menu,
+  MenuItem,
 } from '@material-ui/core'
 import { PlayArrow } from '@material-ui/icons'
+import React, { useContext } from 'react'
+import { AppContext, ExampleCode } from '../contexts/AppContext'
 
 /**
  * NavBarProps type.
@@ -16,12 +20,22 @@ export type NavBarProps = {
    * Run button click handler.
    */
   onRunClicked(): void
+
+  /**
+   * Example select handler.
+   *
+   * @param example Example code.
+   */
+  onExampleSelect(example: ExampleCode): void
 }
 
 const useStyles = makeStyles(() =>
   createStyles({
     title: {
       marginRight: '1rem',
+    },
+    navItem: {
+      margin: '0 0.5rem',
     },
     grow: {
       flexGrow: 1,
@@ -34,6 +48,21 @@ const useStyles = makeStyles(() =>
  */
 export const NavBar: React.FC<NavBarProps> = (props): React.ReactElement => {
   const classes = useStyles()
+  const { examples } = useContext(AppContext)
+
+  const [
+    exampleAnchorEl,
+    setExampleAnchorEl,
+  ] = React.useState<null | HTMLElement>(null)
+
+  const onExampleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setExampleAnchorEl(event.currentTarget)
+  }
+
+  const onExampleSelect = (example: ExampleCode) => {
+    setExampleAnchorEl(null)
+    props.onExampleSelect(example)
+  }
 
   return (
     <>
@@ -45,9 +74,45 @@ export const NavBar: React.FC<NavBarProps> = (props): React.ReactElement => {
           <div className={classes.grow}></div>
           <Button
             variant="contained"
+            color="default"
+            aria-controls="example-menu"
+            aria-haspopup="true"
+            onClick={onExampleClick}
+            className={classes.navItem}
+          >
+            Examples
+          </Button>
+          <Menu
+            id="example-menu"
+            anchorEl={exampleAnchorEl}
+            keepMounted
+            getContentAnchorEl={null}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'center',
+            }}
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'center',
+            }}
+            open={Boolean(exampleAnchorEl)}
+            onClose={onExampleSelect}
+          >
+            {examples.map((example) => (
+              <MenuItem
+                key={example.title}
+                onClick={() => onExampleSelect(example)}
+              >
+                {example.title}
+              </MenuItem>
+            ))}
+          </Menu>
+          <Button
+            variant="contained"
             color="secondary"
             startIcon={<PlayArrow />}
             onClick={props.onRunClicked}
+            className={classes.navItem}
           >
             Run(Ctrl + Enter)
           </Button>
