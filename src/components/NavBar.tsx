99f59e4ce95b492811e737a08cb85ce967ace83a
hota1024 <hotalog@hota1024.com>
2020-09-09 11:57:05 +0900

    🏷️ Add Props

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -8,6 +8,16 @@ import {
 } from '@material-ui/core'
 import { PlayArrow } from '@material-ui/icons'
 
+/**
+ * NavBarProps type.
+ */
+export type NavBarProps = {
+  /**
+   * Run button click handler.
+   */
+  onRunClicked(): void
+}
+
 const useStyles = makeStyles(() =>
   createStyles({
     title: {
@@ -19,7 +29,7 @@ const useStyles = makeStyles(() =>
 /**
  * NavBar component.
  */
-export const NavBar = (): React.ReactElement => {
+export const NavBar: React.FC<NavBarProps> = (props): React.ReactElement => {
   const classes = useStyles()
 
   return (
@@ -33,6 +43,7 @@ export const NavBar = (): React.ReactElement => {
             variant="contained"
             color="secondary"
             startIcon={<PlayArrow />}
+            onClick={props.onRunClicked}
           >
             Run(Ctrl + Enter)
           </Button>
