111d8f02d826e966e990cd796b706d20925cbade
hota1024 <hotalog@hota1024.com>
2020-09-10 15:51:59 +0900

    🏷️ Add apis

--- src/components/ApiDocument.tsx
+++ src/components/ApiDocument.tsx
@@ -6,7 +6,7 @@ import { makeStyles } from '@material-ui/core'
 export type Syntax = {
   syntax: string
 
-  example: string
+  description: string
 }
 
 const useStyles = makeStyles(() => ({
@@ -34,8 +34,26 @@ const useStyles = makeStyles(() => ({
 export const ApiDocument: React.FC = () => {
   const syntaxes: Syntax[] = [
     {
-      syntax: 'pg.log(message: any)',
+      syntax: 'pg.log(message: any): void',
       description: 'Add the message to the Logs tab.',
+    },
+    {
+      syntax: 'pg.alert(message: string): Promise',
+      description: 'Show playground alert.',
+    },
+    {
+      syntax: 'AskFn<T>',
+      description: 'A function that returns Promise<T>.',
+    },
+    {
+      syntax: 'pg.useAskString(message: string): AskFn<string>',
+      description:
+        'Returns a function that returns string value entered by user.',
+    },
+    {
+      syntax: 'pg.useAskNumber(message: string): AskFn<number>',
+      description:
+        'Returns a function that returns number value entered by user.',
     },
   ]
 
